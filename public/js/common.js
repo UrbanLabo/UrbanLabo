f77cb694e21e774906e695fd4e3e7113e73a7845
FunoMue <UrbanLaboForUEA@gmail.com>
2021-08-04 21:17:20 +0900

    make Procfile

---
+++ public/js/common.js
@@ -0,0 +1,72 @@
+$(function(){
+	$('.about-using-data').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('使用している<br>データについて');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.2020').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('令和2<br/>（2020）年');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.2010').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('平成22<br/>（2010）年');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.2005').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('平成17<br/>（2005）年');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.2000').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('平成12<br/>（2000）年');
+		$(this).css({color: "#986DB2"});
+	});
+    $('.1995').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('平成7<br/>（1995）年');
+		$(this).css({color: "#986DB2"});
+	});
+    $('.1990').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('平成2<br/>（1990）年');
+		$(this).css({color: "#986DB2"});
+	});
+    $('.1980').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('昭和55<br/>（1980）年');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.ranking').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('都市雇用圏<br>ランキング');
+		$(this).css({color: "#986DB2"});
+	});
+	$('.download').hover(function(){
+		$(this).html('準備中');
+		$(this).css({color: "white"});
+	},function(){
+		$(this).html('ダウンロード');
+		$(this).css({color: "#986DB2"});
+	});
+});
